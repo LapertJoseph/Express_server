564cd1f5a9bdf66148a70a170ab6f691eba06624
LapertJoseph <lapertjoseph@gmail.com>
2022-12-06 09:33:13 +0100

    ajout controller insert

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -15,4 +15,17 @@ module.exports = {
         if (connection) connection.end();
     }
     },
+    insertUser: async (req, res) => {
+        let connection;
+        const { name, phone, email, address, postalZip, region, country, list, alphanumeric, currency, numberrange, text } = req.body
+        try {
+            connection = await pool.getConnection();
+            const result = await connection.query('CALL insert_user(?,?,?,?,?,?,?,?,?,?,?,?);', [name, phone, email, address, postalZip, region, country, list, alphanumeric, currency, numberrange, text]);
+            return res.status(200).json({ success: result });
+        } catch (error) {
+            return res.status(400).json( {error: error.message}); 
+        } finally {
+            if (connection) connection.end();
+        };
+    },
 }
