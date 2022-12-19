b8dc0e7353c7fae47550b6e28eaf5286e1d06b7d
LapertJoseph <lapertjoseph@gmail.com>
2022-12-19 23:11:57 +0100

    ajout de la route associé au select user

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,20 +1,13 @@
 const router = require('express-promise-router')();
 
 
-const {
-    selectAllUser, 
-    insertUser,
-    deleteUser,
-    updateUser,
-} = require('../controllers/user_controller');
+const { selectAllUser } = require('../controllers/user_controller');
 
 router
     .route('/')
 router
     .route('/users')
     .get(selectAllUser)
-    .post(insertUser)
-    .delete(deleteUser)
-    .put(updateUser)
+    
     
 module.exports = router;   
