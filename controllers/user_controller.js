1d78bfbc38e05e8460ef1b10d84285ad1916cc40
LapertJoseph <lapertjoseph@gmail.com>
2022-12-19 23:56:59 +0100

    ajout controller pour post user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -6,8 +6,21 @@ module.exports = {
     try {
       connection = await pool.getConnection();
       const data = await connection.query("CALL get_all_users;");
-      console.log(data);
       return res.status(200).json({ success: true, data: data[0] });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    } finally {
+      if (connection) connection.end();
+    }
+  },
+  postUser: async (req, res) => {
+    let connection;
+    try {
+      const {firstName, lastName, mobile, passwordHash, admin, vendor, registeredAt} = req.body;
+      connection = await pool.getConnection();
+      const data = await connection.query("CALL post_user(?,?,?,?,?,?,?);", [firstName, lastName, mobile, passwordHash, admin, vendor, registeredAt]);
+      console.log(data);
+      return res.status(200).json({ success: true, data: data });
     } catch (error) {
       return res.status(400).json({ error: error.message });
     } finally {
