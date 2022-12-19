5f13307cd39064b30468ccb9b45169f9b51e3560
LapertJoseph <lapertjoseph@gmail.com>
2022-12-19 23:57:19 +0100

    ajout de la route lié au post user

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,13 +1,14 @@
 const router = require('express-promise-router')();
 
 
-const { selectAllUser } = require('../controllers/user_controller');
+const { selectAllUser, postUser } = require('../controllers/user_controller');
 
 router
     .route('/')
 router
     .route('/users')
     .get(selectAllUser)
+    .post(postUser)
     
     
 module.exports = router;   
