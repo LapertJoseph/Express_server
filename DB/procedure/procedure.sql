20bb0302e3ad37365c20a0345a3be774366d5daa
LapertJoseph <lapertjoseph@gmail.com>
2022-12-22 21:06:37 +0100

    modif query for delete user

--- DB/procedure/procedure.sql
+++ DB/procedure/procedure.sql
@@ -34,7 +34,9 @@ BEGIN
     SET firstName = p_firstName, lastName = p_lastName, passwordHash = p_passwordHash, `admin` = p_admin, vendor = p_vendor, registeredAt = p_registeredAt;
 END //
 
-CREATE OR REPLACE PROCEDURE delete_user ()
+CREATE OR REPLACE PROCEDURE delete_user (
+    IN p_id INT
+)
 BEGIN
-    DELETE FROM user WHERE id = 52;
+    DELETE FROM user WHERE id = p_id;
 END //
