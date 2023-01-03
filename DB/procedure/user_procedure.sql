c7cfc1fd3683494e6268f3f7b06b3e244bb4568c
LapertJoseph <lapertjoseph@gmail.com>
2023-01-03 08:55:00 +0100

    rename procedure

---
+++ DB/procedure/user_procedure.sql
@@ -0,0 +1,42 @@
+use testdbjoe;
+
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE get_all_users()
+BEGIN
+    SELECT * FROM user LIMIT 50;
+END //
+
+CREATE OR REPLACE PROCEDURE post_user(
+    IN p_firstName VARCHAR(50),
+    IN p_lastName VARCHAR(50),
+    IN p_mobile VARCHAR(15),
+    IN p_passwordHash VARCHAR(32),
+    IN p_admin  TINYINT(1),
+    IN p_vendor TINYINT(1),
+    IN p_registeredAt datetime
+)
+BEGIN
+    INSERT INTO user (firstName, lastName, mobile,  passwordHash, `admin`, vendor, registeredAt) 
+    VALUES (p_firstName, p_lastName, p_mobile, p_passwordHash, p_admin, p_vendor, p_registeredAt);
+END //
+
+CREATE OR REPLACE PROCEDURE update_user (
+    IN p_firstName VARCHAR(50),
+    IN p_lastName VARCHAR(50),
+    IN p_passwordHash VARCHAR(32),
+    IN p_admin  TINYINT(1),
+    IN p_vendor TINYINT(1),
+    IN p_registeredAt datetime
+)
+BEGIN
+    UPDATE user 
+    SET firstName = p_firstName, lastName = p_lastName, passwordHash = p_passwordHash, `admin` = p_admin, vendor = p_vendor, registeredAt = p_registeredAt;
+END //
+
+CREATE OR REPLACE PROCEDURE delete_user (
+    IN p_id INT
+)
+BEGIN
+    DELETE FROM user WHERE id = p_id;
+END //
