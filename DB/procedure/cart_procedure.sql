57c68721c18caaf0864141e8aaf5c6c345118582
LapertJoseph <lapertjoseph@gmail.com>
2023-01-03 09:51:31 +0100

    ajout du crud PUT pour cart

--- DB/procedure/cart_procedure.sql
+++ DB/procedure/cart_procedure.sql
@@ -17,3 +17,16 @@ BEGIN
     INSERT INTO cart (sessionId, token, status, createdAt)
     VALUES (p_sessionId, p_token, p_status, p_createdAt);
 END //
+
+CREATE OR REPLACE PROCEDURE update_cart(
+    IN p_id bigint(20),
+    IN p_sessionId VARCHAR(100),
+    IN p_token VARCHAR(100),
+    IN p_status SMALLINT(6),
+    IN p_createdAt datetime
+)
+BEGIN
+    UPDATE cart 
+    SET sessionId = p_sessionId, token = p_token, status = p_status, createdAt = p_createdAt
+    WHERE id = p_id;
+END //
