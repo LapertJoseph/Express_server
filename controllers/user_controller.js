4582a6031c0b10e963d35400754e3ff7fbe40c85
LapertJoseph <lapertjoseph@gmail.com>
2023-01-10 23:27:00 +0100

    remove the functions that did not match

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,7 +2,6 @@ const jsonwebtoken = require('jsonwebtoken');
 const pool = require("../config/database");
 const Config = require('../config/env');
 
-
 module.exports = {
 
   login: async (req,res) => {
@@ -106,56 +105,4 @@ module.exports = {
       if(connection) connection.end();
     }
   },
-  selectAllCart: async (req, res) => {
-    let connection;
-    try {
-      connection = await pool.getConnection();
-      const data = await connection.query('CALL get_all_cart;');
-      res.status(200).json({success: true, data: data});
-    } catch (error) {
-      res.status(400).json({error: error.message});
-    } finally {
-      if(connection) connection.end();
-    }
-  },
-  postCart: async (req, res) => {
-    let connection;
-    const {sessionId, token, status, createdAt} = req.body;
-    try {
-      connection = await pool.getConnection();
-      const data = await connection.query('CALL post_cart(?,?,?,?);', [sessionId, token, status, createdAt]);
-      res.status(200).json({success: true, data: data});
-    } catch (error) {
-      res.status(400).json({error: error.message});
-    } finally {
-      if(connection) connection.end();
-    }
-  },
-  updateCart: async (req,res) => {
-    let connection;
-    const {sessionId, token, status, createdAt} = req.body;
-    const {id} = req.params;
-    try {
-      connection = await pool.getConnection();
-      const data = await connection.query('CALL update_cart(?,?,?,?,?);', [id, sessionId, token, status, createdAt]);
-      res.status(200).json({success: true, data: data});
-    } catch (error) {
-      res.status(400).json({error: error.message});
-    } finally {
-      if(connection) connection.end();
-    }
-  },
-  deleteCart: async (req, res) => {
-    let connection;
-    const {id} = req.params;
-    try {
-      connection = await pool.getConnection();
-      const data = await connection.query('CALL delete_cart(?)', [id]);
-      res.status(200).json({sucess: true , data: data, message: "Cart deleted"})
-    } catch (error) {
-      res.status(400).json({error: error.message});
-    } finally {
-      if(connection) connection.end();
-    }
-  }
 };
