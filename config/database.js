2e5de58e6ed398cd0a58fd4a2b7b5ade330200e8
LapertJoseph <lapertjoseph@gmail.com>
2023-01-10 23:32:02 +0100

    add config to connect DB

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
+/**
+ * Config to connect to mariadb database
+ */
 const mariadb = require('mariadb');
-
+const Config = require('./env');
 
 const pool = mariadb.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: 'abc',
-    database: 'prod',
-    port: 3306
+    host: Config.database.host,
+    user: Config.database.user,
+    password: Config.database.password,
+    database: Config.database.database,
+    port: Config.database.port
     
 })
 
