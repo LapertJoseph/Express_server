a80b27b35ad68f3960d7c9a751703e94c836ef15
LapertJoseph <lapertjoseph@gmail.com>
2023-03-13 23:09:34 +0100

    modif cors

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const route = require('./routes');
-const cors = require('cors')
-const session = require('express-session');
+const route = require("./routes");
+const cors = require("cors");
+const session = require("express-session");
 
 // Function which parse the body on Json with bigInt
-BigInt.prototype.toJSON = function() { return this.toString() }; 
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
 app.use(express.json());
 
-app.use(session({
-    secret: 'Jean-michel$123456789azertyzza',
+app.use(
+  session({
+    secret: "Jean-michel$123456789azertyzza",
     saveUninitialized: false,
     resave: false,
-    cookie: { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 }
-}));
+    cookie: { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
 
 const PREFIX = "/";
 
 if (process.env.NODE_ENV === "dev") {
-    if (process.env.NODE_ENV === 'dev') {
-        app.use(cors({
-            origin : "http://localhost:3000",
-            credentials: true
-        }))
-    }
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
 }
 app.use(PREFIX, route);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`server listening on ${port}`);
+  console.log(`server listening on ${port}`);
 });
